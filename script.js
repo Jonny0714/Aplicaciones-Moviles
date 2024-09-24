6f8b76e08f8885b7daaad61dda42757b6b2b2540
Jonny0714 <madrazo.rivera.jonathan@gmail.com>
2024-09-23 20:45:15 -0600

    Trabajo del 23/09/2024

---
+++ script.js
@@ -0,0 +1,33 @@
+//18/09/24 / Madrazo Rivera Jonathan
+//comentario en una sola línea
+/*Comentario
+en mutilineas */
+
+
+//Variables
+/*
+Las variables guardan información reutilizable
+*/
+
+
+//Declaración de las variables
+let nombre = "Jonathan"
+const PI = 3.1416;
+var edad = 16;
+
+
+/*Tipos de datos
+Cadena de texto(String): "Hola",'mundo'
+Número(number): 5,13,14
+Booleanos (boolean):(true , false)
+*/
+
+
+//Ejemplo
+let mensaje = 'Hola Mundo'
+let numero = 42;
+let Estudiante = true;
+
+
+//Operadores matemáticos
+let a = 5;
